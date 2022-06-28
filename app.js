e945d8e72239bae4df9f7b5fb56482b59c03b288
Shantanu Jana <omni.7x@gmail.com>
2022-06-28 23:34:02 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const display = document.querySelector('.container');
-
+// displaying grid box
 for(let row = 0; row < 256; row++) {
     const box = document.createElement('div');
     box.setAttribute('class', 'grid-box');
@@ -7,7 +7,16 @@ for(let row = 0; row < 256; row++) {
 
 }
 
+let gridBox = document.querySelectorAll('.grid-box');
 
-// adding random colors to divs elements: 
-const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+gridBox.forEach(function (box) {
+    // adding random colors to divs elements: 
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    box.addEventListener('mouseover', function (event) {
+       event.target.style.backgroundColor = `#${randomColor}`;
+    });
+});
+
+
+
 
